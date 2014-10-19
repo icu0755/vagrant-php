ebc708ada311f212c421b1b62bb9193df3916cbc
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2014-10-19 19:43:47 +0400

    Remove old mysql resources

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -15,20 +15,7 @@ class { 'git': }
 
 class { 'mc': }
 
-class { 'mysql': 
-    template => "mysql/mysql.conf.erb",
-    root_password => 'root',
-}
-
 apache::module { 'rewrite': }
-
-mysql::grant { 'allow remote root':
-  mysql_privileges => 'ALL',
-  mysql_password => 'root',
-  mysql_db => '*',
-  mysql_user => 'root',
-  mysql_host => '%',
-}
 
 include php
 class {
