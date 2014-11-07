69cbfb99bfcd24e5fb829ca550afd1cf3d60ab7f
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2014-11-07 20:15:36 +0400

    [MOV] php settings to a separate file php-apache.pp

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -10,48 +10,14 @@ class { 'mc': }
 
 apache::module { 'rewrite': }
 
-include php
-class {
-    # Base packages
-    [ 'php::apache' ]:
-      ensure => installed;
-
-    # PHP extensions
-    [
-      'php::extension::curl', 'php::extension::gd', 'php::extension::mcrypt', 'php::extension::mysql', 'php::extension::opcache'
-    ]:
-      ensure => installed;
-
-    # xdebug
-    'php::extension::xdebug':
-      ensure => installed,
-      settings => [
-        "set .anon/zend_extension 'xdebug.so'",
-        "set .anon/xdebug.remote_enable '0'",
-        "set .anon/xdebug.remote_connect_back '1'",
-      ]
-
-  }
-
-# Fixing ubuntu bug
-file {'/etc/php5/apache2/conf.d/20-mcrypt.ini':
-    notify => Service[apache],
-    ensure => link,
-    target => '../../mods-available/mcrypt.ini',
-    require => Package['php5-mcrypt'],
-}
-
-file {'/etc/php5/cli/conf.d/20-mcrypt.ini':
-    ensure => link,
-    target => '../../mods-available/mcrypt.ini',
-    require => Package['php5-mcrypt'],
-}
-
-
 class {'composer': }
 
 class {'php::pear':}
+
 class {'php::phpunit':}
 
 import 'vhost.pp'
+
 import 'mysql.pp'
+
+import 'php-apache.pp'
